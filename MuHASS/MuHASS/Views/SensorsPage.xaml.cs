46408263b6098b2741e839b5ed23f70b00d64da5
WEAR <WEAR@UCF.EDU>
2022-09-16 16:17:53 -0400

    Updated UI

---
+++ MuHASS/MuHASS/Views/SensorsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MuHASS.Views
+{
+  public partial class SensorsPage : ContentPage
+  {
+    public SensorsPage()
+    {
+      InitializeComponent();
+    }
+  }
+}
