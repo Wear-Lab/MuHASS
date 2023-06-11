8edaa5ef6b1dec4ebf7f4f9839de1918c5eeeed8
promise310 <seoyoung22dev@gmail.com>
2023-06-11 01:45:18 -0700

    (Final)Set up Connect and Raw Data pages

--- MuHASS-App/src/components/RawData.jsx
+++ MuHASS-App/src/components/RawData.jsx
@@ -1,11 +1,42 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const RawData = () => {
   return (
     <View>
       <Text>Raw Data</Text>
+
+      <View style={styles.container}>
+
+      <Text> </Text>
+      <Text>Accel X: ### Y: ### Z: ###</Text>
+      <Text>Gyro X: ### Y: ### Z: ###</Text>
+      <Text>Mag X: ### Y: ### Z: ###</Text>
+      <Text> </Text>
+      <Text>Temp:</Text>
+      <Text>Press:</Text>
+      <Text>Humid:</Text>
+      <Text> </Text>
+      <Text>Light:</Text>
+      <Text> </Text>
+      <Text>Altitude:</Text>
+      <Text> </Text>
+      <Text>HR:</Text>
+      <Text>Sp02:</Text>
+      <Text>GSR:</Text>
+      <Text></Text>
+      <Text></Text>
     </View>
+  </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'F1F1F1',
+    justifyContent: 'center',
+    alignItems: 'flex',
+  }
+});
+
 
 export default RawData;
