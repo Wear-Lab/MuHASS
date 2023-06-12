57fd2ea9676f8db2593aa5959224d84bf889476d
promise310 <seoyoung22dev@gmail.com>
2023-06-11 17:17:15 -0700

    Completed Connect Page

--- MuHASS-App/src/components/connect.jsx
+++ MuHASS-App/src/components/connect.jsx
@@ -1,17 +1,57 @@
 import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { SelectList } from 'react-native-dropdown-select-list'
 
 const connect = () => {
-    return(
-        <View>
-            <Text>Connect Page</Text>
-        </View>
-    );
+
+  const [selected, setSelected] = React.useState("");
+  
+  const data = [
+      {key:'1', value:'Wifi 1'},
+      {key:'2', value:'Wifi 2'},
+      {key:'3', value:'Wifi 3'},
+      {key:'4', value:'Wifi 4'},
+  ]
+
+  return(
+    <View style = {styles.container}>
+    <SelectList 
+        setSelected={(val) => setSelected(val)} 
+        data={data} 
+        save="value"
+        // setSelected={setSelected} data={data}
+    />
+    </View>
+  )
+
+  // const [data,setData] = React.useState([]);
+  
+  // React.useEffect(() => 
+  //   //Get Values from database
+  //   axios.get('https://jsonplaceholder.typicode.com/users')
+  //     .then((response) => {
+  //       // Store Values in Temporary Array
+  //       let newArray = response.data.map((item) => {
+  //         return {key: item.id, value: item.name}
+  //       })
+  //       //Set Data Variable
+  //       setData(newArray)
+  //     })
+  //     .catch((e) => {
+  //       console.log(e)
+  //     })
+  // ,[])
+
+  // return(
+  //   <SelectList setSelected={setSelected} data={data} onSelect={() => alert(selected)} />
+  // )
+
 };
+
 const styles = StyleSheet.create({
     container: {
-      backgroundColor: 'F1F1F1',
-      justifyContent: 'center',
-      alignItems: 'flex',
+      height: 100, 
+      width:400,
     }
   });
 
