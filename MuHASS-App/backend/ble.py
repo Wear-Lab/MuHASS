2c202c97cc8ddae83b5bf63d7e62ac3ba4fd353c
Sean Feldman <the.feldman23@gmail.com>
2023-06-20 21:38:58 -0400

    BLE scan devices

--- MuHASS-App/backend/ble.py
+++ MuHASS-App/backend/ble.py
@@ -1,37 +1,42 @@
 import asyncio
 from bleak import BleakScanner
+from bleak import BleakClient
 from fastapi import FastAPI
-import pyfirmata
 import uvicorn
+
+# We want to scan for devices 
+# then connect to the MAC address or UUID we specify
+# After that, we scan for that device's services 
+# and access the characteristics of specified service(s)
 
 
 # Bluetooth device data
 bluetoothData = {
-    "devices": []
+    "devices": [],
 }
-
-# board = pyfirmata.Arduino()
-
 
 # API
 
 app = FastAPI()
 
-# Path to user's available devices
-@app.get("/")
-async def getDevices():
-    return bluetoothData["devices"] 
 
 # Scanning available devices
 async def findDevices():
     devices = await BleakScanner.discover(timeout=5)
     for device in devices:
+        device = str(device)
         print("Device: ", device)
         bluetoothData["devices"].append(device)
+
 
 @app.on_event("startup")
 async def startup_eventj():
     await findDevices()
+
+# Path to user's available devices
+@app.get("/")
+async def getDevices():
+    return bluetoothData["devices"] 
 
 
 if __name__ == "__main__":
