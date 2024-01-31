3e89092c6e3e4dc862845031b245570a75591fa6
Seoyoung Kong <seoyoung22dev@gmail.com>
2024-01-30 22:05:35 -0500

    Added Data and Calculated

--- backend/server.ts
+++ backend/server.ts
@@ -82,18 +82,6 @@ app.get('/userInfo', async (req, res) => {
 app.post('/userInfo', async (req, res) => {
     const { userId, userInfo }: { userId: number, userInfo: UserInfo; } = req.body;
 
-    /* Test User Info Data */
-    // const userInfo = await UserInfo({
-    //     ENMO: '34',
-    //     HR: '123',
-    //     SpO2: '123',
-    //     GSR: '123',
-    //     LPA: '123',
-    //     MVPA: '123'
-    // });
-    // await userInfo.save();
-    // res.json(userInfo);
-
     const user = await prisma.userInfo.create({
         data: {
             ...userInfo,
@@ -131,18 +119,136 @@ app.delete('/userInfo', async (req, res) => {
     res.status(200).json(deleted);
 });
 
-app.post('/environment-user', async (req, res) => {
-    /* Test User Environment Data */
-    // const userEnvironment = await Environment({
-    //     Temperature: "34",
-    //     Pressure: "123",
-    //     Humidity: "123",
-    //     Altitude: "123"
-    // });
-    // await userEnvironment.save();
-    // res.json(userEnvironment);
 
-    res.json(req.body);
+//User data(Raw data)
+interface Data {
+    acceleration: number;
+    magnetic: number;
+    gyroscope: number;
+    temperature: number;
+    pressure: number;
+    altitude: number;
+    humidity: number;
+    gsr: number;
+    ppg: number;
+    hr: number;
+    microphone: number;
+    k: number;
+    time: DateTime;
+}
+  
+
+app.get('/data', async (req, res) => {
+    const { userId } = req.body;
+
+    const user = await prisma.data.findUnique({
+        where: {
+            userId: userId,
+        }
+    });
+
+    res.status(200).json(user);
+});
+
+app.post('/data', async (req, res) => {
+    const { userId, data }: { userId: number, data: Data; } = req.body;
+
+    const user = await prisma.data.create({
+        data: {
+            ...data,
+            userId: userId,
+        }
+    });
+
+    res.status(200).json(user);
+});
+
+app.put('/data', async (req, res) => {
+    const { userId, data }: { userId: number, data: Data; } = req.body;
+
+    const user = await prisma.data.update({
+        where: {
+            userId: userId,
+        },
+        data: {
+            ...data,
+        }
+    });
+
+    res.status(200).json(user);
+});
+
+app.delete('/data', async (req, res) => {
+    const { userId } = req.body;
+
+    const deleted = await prisma.data.delete({
+        where: {
+            userId: userId,
+        }
+    });
+
+    res.status(200).json(deleted);
+});
+
+
+//Calculated data 
+interface Calculated {
+    enmo: number;
+    lvpa: number;
+    mvpa: number;
+    time: DateTime;
+}
+
+app.get('/calculated', async (req, res) => {
+    const { userId } = req.body;
+
+    const user = await prisma.calculated.findUnique({
+        where: {
+            userId: userId,
+        }
+    });
+
+    res.status(200).json(user);
+});
+
+app.post('/calculated', async (req, res) => {
+    const { userId, calculated }: { userId: number, calculated: Calculated; } = req.body;
+
+    const user = await prisma.calculated.create({
+        data: {
+            ...calculated,
+            userId: userId,
+        }
+    });
+
+    res.status(200).json(user);
+});
+
+app.put('/calculated', async (req, res) => {
+    const { userId, calculated }: { userId: number, calculated: Calculated; } = req.body;
+
+    const user = await prisma.calculated.update({
+        where: {
+            userId: userId,
+        },
+        data: {
+            ...calculated,
+        }
+    });
+
+    res.status(200).json(user);
+});
+
+app.delete('/calculated', async (req, res) => {
+    const { userId } = req.body;
+
+    const deleted = await prisma.calculated.delete({
+        where: {
+            userId: userId,
+        }
+    });
+
+    res.status(200).json(deleted);
 });
 
 
